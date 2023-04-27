c7a85ac2748bb99d606503ffcd82eacb89f7b4ff
Sreevani Jarugula <jsv1206@gmail.com>
2023-04-27 19:56:34 +0000

    moving functions to utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
 #import packages used for generating subject set
-
 import matplotlib.pyplot as plt
 import gc
 import numpy as np
 import pandas
+import pandas as pd
 import time
+import os
 
 # Astropy imports
 from astropy.wcs import WCS
@@ -36,6 +37,78 @@ plt.style.use('tableau-colorblind10')
 import warnings
 from astropy.units import UnitsWarning
 
+pd.set_option('display.max_rows', 20)
+warnings.simplefilter("ignore", category=UnitsWarning)
+
+plot_filter_labels = {'u':'u', 'g':'g', 'r':'r', 'i':'i', 'z':'z', 'y':'y'}
+plot_filter_colors = {'u': '#56b4e9', 'g': '#008060', 'r': '#ff4000',
+                      'i': '#850000', 'z': '#6600cc', 'y': '#000000'}
+plot_filter_symbols = {'u': 'o', 'g': '^', 'r': 'v', 'i': 's', 'z': '*', 'y': 'p'}
+    
+def get_cutout_image(butler, ra_deg, dec_deg, visit, detector, band, cutoutSideLength, datasetType='calexp'):
+    """
+    Get the cutout image information from butler. 
+    This shoudl be followed by make_fig
+
+    Input Parameters
+    ----------
+    ra : ra of source in degrees
+    dec : dec of source in degrees
+    visit : visit id
+    detector : detector number
+    band : band to get cutput for
+    cutoutSideLength : size of the cutout
+    
+    Returns
+    ----------
+    Cutout image information
+    """
+    cutoutSize = geom.ExtentI(cutoutSideLength, cutoutSideLength)
+    
+    radec = geom.SpherePoint(ra_deg,dec_deg, geom.degrees)
+    
+    dataId = {'visit': visit, 'detector': detector}  
+    calexp_wcs = butler.get('calexp.wcs', **dataId)
+    
+    xy = geom.PointI(calexp_wcs.skyToPixel(radec))
+    bbox = geom.BoxI(xy - cutoutSize // 2, cutoutSize)
+    parameters = {'bbox': bbox}
+    
+    cutout_image = butler.get('calexp', parameters=parameters, **dataId)
+
+    return cutout_image
+
+def make_calexp_fig(cutout_image, ra, dec, out_name):
+    """
+    Create an image.
+    should be followed with remove_figure
+    
+    Parameters
+    ----------
+    cutout_image : cutout_image from butler.get
+    ra : ra of source in degrees
+    dec : dec of source in degrees
+    out_name : file name where you'd like to save it
+    
+    Returns
+    ----------
+    cutout image
+    """
+    fig = plt.figure(figsize=(4, 4))
+    afw_display = afwDisplay.Display(frame=fig)
+    afw_display.scale('asinh', 'zscale')
+    afw_display.mtv(cutout_image.image)
+    
+    cutout_wcs = cutout_image.getWcs()
+    radec = geom.SpherePoint(ra, dec, geom.degrees)
+    xy = geom.PointI(cutout_wcs.skyToPixel(radec))
+    
+    afw_display.dot('x', xy.getX(), xy.getY(), size=1, ctype='orange')
+    plt.gca().axis('off')
+    plt.savefig(out_name)
+    
+    return fig
+
 
 def remove_figure(fig):
     """
@@ -56,6 +129,58 @@ def remove_figure(fig):
     plt.close(fig)  # close the figure
 
     gc.collect()    # call the garbage collector
+    
+def get_flux(flux_table):
+    """
+    Create dictionary of light curve.
+    This should be follwed by plotlc
+
+    Input Parameters
+    ----------
+    flux_table : from query_flux
+    
+    Returns
+    ----------
+    two dictionaries of days in MJD and flux for each band
+    """
+    pick = {}
+    for filter in plot_filter_labels:
+        pick[filter] = (flux_table['band'] == filter)
+    mjd_days = {}
+    mags = {}
+    for filter in plot_filter_labels:
+        mjd_days[filter] = np.array(flux_table[pick[filter]]['expMidptMJD']) * u.day
+        mags[filter] = np.array(flux_table[pick[filter]]['psfMag'])
+        
+    return mjd_days, mags
+    
+def plotlc(bands, days, magnitudes, out_name):
+    """
+    Create a light curve.
+
+    Input Parameters
+    ----------
+    days : dictionary for MJD in each band 
+    magnitudes : dictionary for flux in each band
+    out_name : file name where you'd like to save it
+    
+    Returns
+    ----------
+    light curve image
+    """
+    
+    fig = plt.figure(figsize=(10,4))
+    for band in bands:
+        plt.plot(days[band], magnitudes[band],\
+                 plot_filter_symbols[band], ms=4, label=plot_filter_labels[band])
+    plt.minorticks_on()
+    plt.xlabel('MJD (days)')
+    plt.ylabel('magnitude')
+    plt.legend('upper right')
+    plt.legend()
+    plt.savefig(out_name)
+    return fig
+    
     
 def make_figure(exp, out_name):
     """
@@ -133,10 +258,6 @@ def setup_butler(config, collection):
     assert service is not None
     assert service.baseurl == "https://data.lsst.cloud/api/tap"
 
-    
-
-    #config = 'dp02'
-    #collection = '2.2i/runs/DP0.2'
     butler = dafButler.Butler(config, collections=collection)
     skymap = butler.get('skyMap')
     
