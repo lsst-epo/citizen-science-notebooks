58bce2d1d7d9e9347bf8414d8d1482d04410ba92
beckynevin <rnevin@fnal.gov>
2024-10-15 13:40:58 +0000

    testing double backslash

--- utils.py
+++ utils.py
@@ -361,12 +361,12 @@ def make_manifest_with_deepcoadd_images(results_table, butler, batch_dir):
         if hasattr(row, "objectId"):
             has_canonical_id = True
             csv_row["objectId"] = row.objectId
-            csv_row["#metadata:objectId"] = row.objectId
+            csv_row["//metadata:objectId"] = row.objectId
             csv_row["objectIdType"] = "DIRECT"
         if hasattr(row, "diaObjectId"):
             has_canonical_id = True
             csv_row["diaObjectId"] = row.diaObjectId
-            csv_row["#metadata:diaObjectId"] = row.diaObjectId
+            csv_row["//metadata:diaObjectId"] = row.diaObjectId
             if "objectIdType" not in csv_row:
                 csv_row["objectIdType"] = "INDIRECT"
 
