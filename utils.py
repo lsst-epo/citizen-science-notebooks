bdaa67fe8a88a905cce18303051c2bcb768e04dd
Brian Nord <184985+bnord@users.noreply.github.com>
2025-08-25 10:20:44 -0500

    Update utils.py flake 8 fixes

--- utils.py
+++ utils.py
@@ -30,11 +30,11 @@ plt.style.use("tableau-colorblind10")
 pd.set_option("display.max_rows", 20)
 warnings.simplefilter("ignore", category=UnitsWarning)
 
-plot_filter_labels = {"u": "u", "g": "g", "r": "r", "i": "i", "z": "z", "y": "y"}
-plot_filter_colors = {'u': '#0c71ff', 'g': '#49be61', 'r': '#c61c00', 'i': '#ffc200', 'z': '#f341a2', 'y': '#5d0000'}
+plot_filter_labels = {"u": "u", "g": "g", "r": "r", "i": "i",
+                      "z": "z", "y": "y"}
+plot_filter_colors = {'u': '#0c71ff', 'g': '#49be61', 'r': '#c61c00', 
+                      'i': '#ffc200', 'z': '#f341a2', 'y': '#5d0000'}
 plot_filter_symbols = {'u': 'o', 'g': '^', 'r': 'v', 'i': 's', 'z': '*', 'y': 'p'}
-
-
 
 
 def get_cutout_image(
